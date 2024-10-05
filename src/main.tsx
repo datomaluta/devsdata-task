860dbc320f577a6a13d222b7245a9b93e8cf5807
datomaluta <datomaluta@gmail.com>
2024-10-05 04:10:03 +0400

    fix: remove useless codes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
 
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
